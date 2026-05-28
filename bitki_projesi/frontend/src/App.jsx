7fa9131715393ce076d2d1f5b47e404bef93df20
Mervenur37 <154545428+Mervenur37@users.noreply.github.com>
2026-05-28 15:57:40 +0300

    Add LandingPage and update routing in App component

--- bitki_projesi/frontend/src/App.jsx
+++ bitki_projesi/frontend/src/App.jsx
@@ -3,19 +3,22 @@ import { AnimatePresence } from 'framer-motion'
 import Navbar from './components/Navbar'
 import AnalysisPage from './pages/AnalysisPage'
 import MetricsPage from './pages/MetricsPage'
+import LandingPage from './pages/LandingPage'
 import { LanguageProvider } from './context/LanguageContext'
 import './App.css'
 
 function App() {
   const location = useLocation()
+  const isLanding = location.pathname === '/'
 
   return (
     <LanguageProvider>
-      <Navbar />
-      <div className="page-container">
+      {!isLanding && <Navbar />}
+      <div className={isLanding ? '' : 'page-container'}>
         <AnimatePresence mode="wait">
           <Routes location={location} key={location.pathname}>
-            <Route path="/" element={<AnalysisPage />} />
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/analyze" element={<AnalysisPage />} />
             <Route path="/metrics" element={<MetricsPage />} />
           </Routes>
         </AnimatePresence>
